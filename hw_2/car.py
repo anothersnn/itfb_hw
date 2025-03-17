4852acdd029e25538374803443b3cbda3f81f311
stef <aar2d1@mail.ru>
2025-03-17 17:57:16 +0300

    download hw_2

---
+++ hw_2/car.py
@@ -0,0 +1,14 @@
+"""
+создайте класс `Car`, наследник `Vehicle`
+"""
+from hw_2.base import Vehicle
+from hw_2.engine import Engine
+
+
+class Car(Vehicle):
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0) -> None:
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
+
+    def set_engine(self, engine: Engine) -> None:
+        self.engine = engine
